bc7f47692ca0b75c52cf15709b6847904c193621
Simon Bertrand <84455908+Simon-Bertrand@users.noreply.github.com>
2023-02-06 23:20:42 +0100

    normalized the convention of preprocessor

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -2,16 +2,15 @@ import pandas as pd
 import numpy as np
 import scipy.stats as st
 
-class preprocessing_score():
+class makePreprocess():
     def __init__(self):
-        self.df = pd.read_csv("preprocessing/statistic_id744823_square-meter-housing-price-in-france-2022-by-type-of-housing.csv",sep=';',usecols=[0,1,2,3])
+        self.df = pd.read_csv("./priori/city_priory.csv",sep=';',usecols=[0,1,2,3])
         self.city = np.array(self.df)[:,0]
         self.price_m2 = np.array(self.df)[:,1]
         self.param = np.array(self.df)[:,-2:]
         
     
-    def ScoreCity(self,df,std=0.1):
-
+    def scoreCity(self,df,std=0.1):
         array_pos = np.array(df[['approximate_latitude','approximate_longitude']])
         Coeff = np.array([st.norm.pdf(array_pos[:,0],loc=self.param[i,0],scale=std)*st.norm.pdf(array_pos[:,1],loc=self.param[i,1],scale=std) for i in range(len(self.param))]).T
         Coeff =  Coeff/np.array([np.sum(Coeff,axis=1)]).T
@@ -22,75 +21,34 @@ class preprocessing_score():
                     Score[idx]=self.price_m2[ydx]
         return Score
     
-    def ScoreSize(self,row):
-        if row['property_type'] in ['appartement','chambre','divers','duplex','loft','maison','villa']:
-            if row['property_type'] == 'appartement' and row['size']>300:
-                return row['size']/10
-            elif row['property_type'] == 'appartement' and row['size']>1000:
-                return row['size']/50
-            elif row['property_type'] == 'appartement' and row['size']>2000:
-                return row['size']/100
-            elif row['property_type'] == 'chambre' and row['size']>100:
-                return row['size']/10
-            elif row['property_type'] == 'divers' and row['size']>10000:
-                return row['size']/10
-            elif row['property_type'] in ['duplex','loft','maison','villa'] and row['size']>2000:
-                return row['size']/10
-            elif np.isnan(row['size']) and (~np.isnan(row['land_size'])) and row['land_size']<300:
-                return row['land_size']
-            elif np.isnan(row['size']) and (np.isnan(row['land_size']) or row['land_size']>=300):
-                return 115 #Median Size
-            else:
-                return row['size']
-        elif row['property_type'] in ['terrain','terrain à bâtir']:
-            return 0
-        elif np.isnan(row['size']) and (~np.isnan(row['land_size'])) and row['land_size']<300: # Attention Ferme ? 
-            return row['land_size']
+
+    def scoreSize(self,row):
+        if row['property_type'] in ['terrain','terrain à bâtir']: return 0
+        elif np.isnan(row['size']) and (~np.isnan(row['land_size'])): return row['land_size']
+        elif np.isnan(row['size']): return 0
+        else: return row["size"]
+
         
-        elif np.isnan(row['size']) and (np.isnan(row['land_size']) or row['land_size']>=300):
-            return 450 
-        else:
-            return row["size"]
-        
-    def ScoreLandsize(self,row):
+    def scoreLandSize(self,row):
         if row['property_type'] in ['appartement','chambre','divers','duplex','loft','maison']:
-            if np.isnan(row['size']) and ~np.isnan(row['land_size']) and row['land_size']<300:
-                return 0
-            elif np.isnan(row['land_size']):
-                return 0
-            else: 
-                return row['land_size']
+            if np.isnan(row['land_size']): return 0
+            else: return row['land_size']
         elif row['property_type'] in ['terrain','terrain à bâtir']:
-            if np.isnan(row['land_size']) and (~np.isnan(row['size'])):
-                return row['size']
-            elif np.isnan(row['land_size']):
-                return 4000 #Mean
-            else:
-                return row['land_size']
-        elif np.isnan(row['size']) and ~np.isnan(row['land_size']) and row['land_size']<300:
-                return 0
-        elif np.isnan(row['land_size']):
-                return 0
-        else:
-            return row['land_size']
+            if np.isnan(row['land_size']) and (~np.isnan(row['size'])): return row['size']
+            elif np.isnan(row['land_size']): return 0
+            else: return row['land_size']
+        elif np.isnan(row['land_size']): return 0
+        else: return row['land_size']
             
 
-    def ScoreFloor(self,row):
-            
-        if np.isnan(row['floor']) or row['floor']==0:
-           return 0
-        else:
-           return np.log((row['floor'])+1)
+    def scoreFloor(self,row):
+        if np.isnan(row['floor']) or row['floor']==0: return 0
+        else: return np.log((row['floor'])+1)
 
     
-    def ScoreRoom(self,row):
-        if np.isnan(row['nb_rooms']):
-            if (~np.isnan(row['size'])):
-                return row['size']/20
-            else:
-                return 1
-        else:
-            return row['nb_rooms']
+    def scoreRoom(self,row):
+        if np.isnan(row['nb_rooms']): return 0
+        else: return row['nb_rooms']
                 
     def scorePropertyType(self,df):
         def minimizeCategoryPropertyType(value):
@@ -105,14 +63,14 @@ class preprocessing_score():
     
         return df.property_type.apply(minimizeCategoryPropertyType)
     
-    def apply_preprocessing(self,df,test=False):
+    def applyPreprocessing(self,df,test=False):
         df_preprocessed = pd.DataFrame([],index=df.index)
         df_preprocessed['scorePropertyType']=self.scorePropertyType(df)
-        df_preprocessed['scoreCity']=self.ScoreCity(df)
-        df_preprocessed['scoreSize']=df.apply(lambda row : self.ScoreSize(row), axis = 1)
-        df_preprocessed['scoreLandsize']=df.apply(lambda row : self.ScoreLandsize(row), axis = 1)
-        df_preprocessed['scoreFloor']=df.apply(lambda row : self.ScoreFloor(row), axis=1)
-        df_preprocessed['scoreRoom']=df.apply(lambda row : self.ScoreRoom(row), axis=1)
+        df_preprocessed['scoreCity']=self.scoreCity(df)
+        df_preprocessed['scoreSize']=df.apply(lambda row : self.scoreSize(row), axis = 1)
+        df_preprocessed['scoreLandsize']=df.apply(lambda row : self.scoreLandsize(row), axis = 1)
+        df_preprocessed['scoreFloor']=df.apply(lambda row : self.scoreFloor(row), axis=1)
+        df_preprocessed['scoreRoom']=df.apply(lambda row : self.scoreRoom(row), axis=1)
         df_preprocessed[['nb_parking_places', 'nb_boxes',
        'has_a_balcony', 'nb_terraces', 'has_a_cellar', 'has_a_garage',
        'has_air_conditioning', 'last_floor', 'upper_floors']]=df[['nb_parking_places', 'nb_boxes',
